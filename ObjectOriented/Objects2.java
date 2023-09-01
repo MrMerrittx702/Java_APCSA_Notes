8c2a5742b5470be7fe71f75a1e90bbbb03ead18f
Evan Merritt <emerritt@almasd.net>
2023-08-31 20:31:50 -0500

    Updated Objects

--- ObjectOriented/Objects2.java
+++ ObjectOriented/Objects2.java
@@ -17,17 +17,49 @@ Using Multiple Classes
 */
 
 
-public class OOP2{
+public class Objects2{
     public static void main(String[] args){
-        OOP thirdObject = new OOP();//created an object "thirdObject" of the class OOP 
-        System.out.println(thirdObject.x + "\n" + thirdObject.y);
 
-        //Here we are accessing the class Methods
-        Methods methodsObject = new Methods();//creating an object of the class "Methods"
-        methodsObject.publicMethod();//calling the public method "publicMethod" 
-        //methods are also accessed through (.) syntax object.method();
-        
-        //it is good practice to create an object of a class and access it in another class. 
+      Objects anotherClassObj = new Objects();
+
+      //Note this will only work if the attributes in the Objects class are not private
+
+      //Accessing Class Attributes from another class (in this case Objects)
+      System.out.println(anotherClassObj.staticNum);     
+      System.out.println(anotherClassObj.staticBool);
+      System.out.println(anotherClassObj.staticDuo);
+      System.out.println(anotherClassObj.staticStr);
+      System.out.println(anotherClassObj.staticStrArray); //prints memory location
+      System.out.println(anotherClassObj.staticStrList);
+      //OR
+      System.out.println(Objects.staticNum);
+      System.out.println(Objects.staticBool);
+      System.out.println(Objects.staticDuo);
+      System.out.println(Objects.staticStr);
+      System.out.println(Objects.staticStrArray); //prints memory location
+      System.out.println(Objects.staticStrList);
+
+      
+      //Accessing object attributes
+      System.out.println(anotherClassObj.num);
+      System.out.println(anotherClassObj.bool);
+      System.out.println(anotherClassObj.duo);
+      System.out.println(anotherClassObj.str);
+      System.out.println(anotherClassObj.strArray); //prints memory location
+      System.out.println(anotherClassObj.strList);
+      
+      //Calling Non Static Methods 
+      anotherClassObj.nonStaticVoidMethod();
+      anotherClassObj.nonStaticVoidMethodWithParameters(1,"default");
+      anotherClassObj.nonStaticMethodWithReturn();
+      anotherClassObj.nonStaticMethodWithReturnAndParameters(1,3);
+      
+      
+      //Calling Static Methods from another class
+      Objects.staticVoidMethod();
+      Objects.staticVoidMethodWithParameters("Java", "Programming");
+      Objects.staticMethodWithReturn();
+      Objects.staticMethodWithReturnAndParameters("static and return");
         
     }
 }
