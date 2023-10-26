b1b316ede53d95f08035a337977133903e16517c
MrMerrittx702 <emerritt@almasd.net>
2023-10-26 11:34:48 -0500

    Organized Notes into Units

--- functional/Test.java
+++
@@ -1,9 +0,0 @@
-package functional;
-
-public class Test {
-  
-  public boolean squirrelPlay(int temp, boolean isSummer) {
-    
-    return ((temp > 59 && (isSummer && temp <=100)) || temp <= 90 );
-  }
-}
