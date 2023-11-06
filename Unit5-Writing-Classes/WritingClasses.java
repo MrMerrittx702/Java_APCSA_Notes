f9edd66915dc55c18e5d3fd26611e74b4cd7867d
MrMerrittx702 <emerritt@almasd.net>
2023-11-05 19:36:00 -0600

    notes update

--- Unit5-Writing-Classes/WritingClasses.java
+++ Unit5-Writing-Classes/WritingClasses.java
@@ -1,18 +1,18 @@
 package objectoriented;
 
-
 /*
 Notes:
 Covered in this file:
 //> Private Attributes and Public Methods
+//> Scoping: local, instance variables, and static variables
 //> Default vs. Private vs. Public Constructors
+//> Copy Constructors
 //> Accessor Methods (getters)
 //> Mutator Methods (setters)
 //> Parameters: Pass by Reference or Value?
 //> Returns: By Reference or Value?
 //> Preconditions and Postconditions
 //> Common Overridden Methods: toString() and equals()
-
  */
 
 public class WritingClasses {
@@ -26,16 +26,64 @@ public class WritingClasses {
   
   private String name;
   private int number;
-  private static String shared = "This value is share amongst all instances of the class.";
+  private static String shared = "This value is shared amongst all instances of the class.";
   
   
   //> Methods are typically public
     //> Encapsulation: provides controlled access to object
     //> Access: allows access to functionality from outside of the class
+
   public void example() {
     return;
   }
-  
+
+
+//============================================================================================================================================================================// 
+// Scoping: local, instance variables, and static variables
+  //> in other languages variables have local and global scopes
+  //> Java has a local scope, but no true global scope
+  //> Scope can be defined as local, instance variable, and static variable
+
+  //> local variables are declared in method bodies, or code blocks (if, while, for, etc.)
+  //> local variables cannot be public/private
+  public static void localScope(String a){
+    //variables accessible here: a
+    
+    boolean b;
+    //variables accessible here: a, b
+    
+    if(true){
+      char c;
+      //variables accessible here: a, b, c
+      
+      while(true){
+        double d;
+        //variables accessible here: a, b, c, d
+
+        for(int e = 0; e < 1; e++){
+          //variables accessible here: a, b, c, d, e
+        }
+        //variables accessible here: a, b, c, d    
+      }
+      //variables accessible here: a, b, c
+    }
+    //variables accessible here: a, b
+  }
+
+  //> instance variables are accessible anywhere the object is accessible
+  //> When a local variable has the same name as an instance variable the local variable is referenced
+  private String var;
+  public void instanceVariables(String var){
+    System.out.println( var == var ); // using the local var
+    System.out.println( this.var == var ); // comparing the instance variable this.var to the local variable var
+  }
+
+  //> static variables are accessible anywhere the class is accessible
+  private static String variable;
+  public static void staticVaraibles(String variable){
+    System.out.println( variable == variable );// using the local variable
+    System.out.println( WritingClasses.variable == variable); //comparing the static variable WrittingClasses.variable to the local variable variable 
+  }
   
 //============================================================================================================================================================================// 
 // Default vs. Private vs. Public Constructors
@@ -56,11 +104,23 @@ public class WritingClasses {
     number = 0;
   }
   
-  //Constructor overloaded with parameters
+  //Constructor overloaded and with parameters
   public WritingClasses( String name, int number ) {
     this.name = name;
     this.number = number;
   }
+
+//============================================================================================================================================================================//  
+// Copy Constructors
+ //> used to make a copy of another Object in the same class
+ //> best practice is to create a copy of the object 
+ //> when a mutable object is a constructor parameter, the instance variable should be initialized with a copy of the referenced object. 
+ 
+ public WritingClasses(WritingClasses obj){
+    this.name = obj.name;
+    this.number = obj.number;
+ }
+ 
 
 //============================================================================================================================================================================// 
 // Accessor Methods (getters)
@@ -94,11 +154,11 @@ public class WritingClasses {
     //> Modifying the passed value does not affect the original
     //> pass a copy of the value
 
-  
-  
-  //> Pass by Value (Reference Types): a copy of the reference(memory location) is passed to the method
+
+  //> Pass by Value (Reference Types): a copy of the reference (memory location) is passed to the method
     //> Modifying the passed reference modifies the original
-    //> Reference Types (Objects) pass a copy of the reference
+    //> Reference Types (Objects) pass a copy of the reference (not a copy of the object)
+    //> It is good practice not to modify mutible objects that are passed as parameters
   
   
   public static void passByValuePrimitive(int para) {
@@ -111,15 +171,54 @@ public class WritingClasses {
     para.name = "New Name";
     System.out.println("para references: " + para);
   }
+
+//============================================================================================================================================================================//  
+// Returns: By Reference or Value?
+  //> returns are return by value (makes a copy)
   
+//============================================================================================================================================================================//  
+// Preconditions and Postconditions
+  //> preconditions are conditions that must be true just prior to the execution of a section of code
+  //> postconditions are conditions that must always be true after the execution of a program
+
+//============================================================================================================================================================================//
+//> Common Overridden Methods: toString() and equals()
+  //> toString() and equals() are defined in the java.lang.Object class
+  //> both are commonly Overridden when writting classes
+
+  //> toString() method is called automatically when an object is passed to System.out.print() and System.out.println()
+  public String toString(){
+    return this.name;
+  }
+
+  //> equals() method is used to define what makes two objects of this class "equal"
+  public boolean equals(Object obj){
+    //check reference equality
+    if( this == obj ){
+      return true;
+    }
+
+    //check class equality
+    if (obj == null || this.getClass() != obj.getClass()){
+      return false;
+    }
+
+    //Cast input object to given class
+    WritingClasses other = (WritingClasses) obj;
+
+    //Check attribute equality
+    return (this.name.equals(other.name) && this.number == other.number); 
+  }
+
+
+
+
   public static void main(String[] args) {
     int num = 0;
     System.out.println("num value before: " + num);
     
     passByValuePrimitive(num);
     System.out.println("num value after: " + num);
-    
-    
     
     WritingClasses obj = new WritingClasses(); 
     System.out.println("obj reference before: " + obj);
@@ -130,48 +229,6 @@ public class WritingClasses {
     System.out.println("obj reference after: " + obj);
     System.out.println("obj.name after: " + obj.name);
   }
- /*
- 
-  
-  
-  
-  * best practice is to create a copy of the object 
-  * when a mutable object is a construcot parameter, the instance variable should be initialized with a copy of the referenced object. 
-  
-
-  * preconditions are conditions tha must be true just prior to the execution of a section of code
-  * postconditions are conditions that must always be true afte the execution of a program
-  * returns are return by value (makes a copy)
-  * toString() method is called when an object is passed to System.out.print() and System.out.println()
-  * mutators often void methods that change the values of instance variables
-  * accessors
-  * good practice not to modify mutible objects that are passed as parameters
-  * primitives are pass by value
-  * reference types are pass by reference
-  * local variables are declared in method bodies and cannot be public/private
-  * when a local variable has the same name as an instance variable the local variable is referenced
-
-  */
-  
-
-  
-  
-  
-  
-  
-
-  
-  //Overridden equals from Objects class
-  public boolean equals(Object obj) {
-    return false;
-  }
-  
-  //Overridden toString() from Objects class
-//  public String toString() {
-//    return name;
-//  }
-  
-  
 
 }
 
--- Unit6-Arrays/CommonArrayAlgorithms.java
+++ Unit6-Arrays/CommonArrayAlgorithms.java
@@ -2,7 +2,7 @@ package functional.processingarrays;
 
 //> Determine min or max value
 //> compute sum, average, or mode
-//> Determine if at least on element has a particular property
+//> Determine if at least one element has a particular property
 //> Determine if all elements have a particular property
 //> Access all consecutive pairs of elements
 //> Determine the precence or abscence of particular elements
@@ -12,4 +12,114 @@ package functional.processingarrays;
 
 public class CommonArrayAlgorithms {
 
+//Find the Maximum from an Array or ArrayList///////////////////////////////
+  public static int findMax(int array){
+    int max = Integer.MIN_VALUE;
+
+    for(int i = 0; i < array.length; i++){
+      if(array[i] > max){
+        max = array[i];
+      }
+    }
+    return max;
+  }
+
+  public static int findMax(ArrayList<Integer> list){
+    int max = Integer.MIN_VALUE;
+
+    for(int i = 0; i < list.size(); i++){
+      if(list.get(i) > max){
+        max = list.get(i);
+      }
+    }
+    return max;
+  }
+
+//////////////////////////////////////////////////////////////////////////////
+
+//Compute Sum of Array or ArrayList///////////////////////////////////////////
+  public static int computeSum(int array){
+    int sum = 0;
+
+    for(int i = 0; i < array.length; i++){
+      sum += array[i];
+    }
+    return sum;
+  }
+
+  public static int computeSum(ArrayList<Integer> list){
+    int sum = 0;
+
+    for(int i = 0; i < list.size(); i++){
+     sum += list.get(i);
+    }
+    return sum;
+  }
+/////////////////////////////////////////////////////////////////////////////////
+
+//Compute Average////////////////////////////////////////////////////////////////
+  public static int computeAvg(int array){
+    int sum = 0;
+
+    for(int i = 0; i < array.length; i++){
+      sum += array[i];
+    }
+    return max/array.length;
+  }
+
+  public static int computeAvg(ArrayList<Integer> list){
+    int sum = 0;
+
+    for(int i = 0; i < list.size(); i++){
+     sum += list.get(i);
+    }
+    return sum/list.size();
+  }
+///////////////////////////////////////////////////////////////////////////////////////
+
+//Determine Mode///////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////
+//Determine if at least one element has a particular property//////////////////////////
+  public static boolean checkPropertyAtLeastOne(boolean array){
+    boolean property = true;
+    for(int i = 0; i < array.length; i++){
+      if(array[i] == property){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static int checkPropertyAtLeastOne(ArrayList<Boolean> list){
+    boolean property = true;
+    for(int i = 0; i < list.size(); i++){
+     if(list.get(i) == property){
+      return true;
+     }
+    }
+    return false;
+  }
+////////////////////////////////////////////////////////////////////////////////////////////
+//Check if all elements have a particular property//////////////////////////////////////////
+  public static boolean checkPropertyAll(boolean array){
+    boolean property = true;
+    for(int i = 0; i < array.length; i++){
+      if(array[i] != property){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static int checkPropertyAll(ArrayList<Boolean> list){
+    boolean property = true;
+    for(int i = 0; i < list.size(); i++){
+     if(list.get(i) != property){
+      return false;
+     }
+    }
+    return true;
+  }
+//////////////////////////////////////////////////////////////////////////////////////////////////
 }
